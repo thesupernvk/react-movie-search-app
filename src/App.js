bd52d9ce2f32c94df1c1f4be9205b82a34805d07
Navin Vijaykumar <navin.vijaykumar@accenture.com>
2022-12-13 17:22:51 +0530

    changes to deploy app on github

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
-const API_URL = "http://www.omdbapi.com?apikey=b8437311";
+const API_URL = "https://www.omdbapi.com?apikey=b8437311";
 
 function App() {
 
